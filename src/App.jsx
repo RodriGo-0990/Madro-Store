ed819cd279d7e8e0ccd99f3da3891bcbc3870bc2
rodrigo.rodrigues <rodrigorodrigues_0990@hotmail.com>
2023-12-04 22:29:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header'
+import Section from "./components/section";
+import Footer from './components/footer';
+import "./index.css"
+
+function App() {
+
+  return (
+   
+      <><Header /><Section/><Section /><Section /><Footer /></>
+    
+  )
+}
+
+export default App
